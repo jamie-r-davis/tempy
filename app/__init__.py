1422ffdd68926712a711a60b6fe9d8d67cd25a5f
Jamie Davis <jamjam@umich.edu>
2020-11-25 21:58:00 -0500

    remove user registration

--- app/__init__.py
+++ app/__init__.py
@@ -57,20 +57,20 @@ def create_app(config=Config):
         if app.config["MAIL_SERVER"]:
             auth = None
             if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-                auth = (auth.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+                auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
             secure = None
             if app.config["MAIL_USE_SSL"]:
                 secure = ()
             mail_handler = SMTPHandler(
                 mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
-                fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+                fromaddr=app.config["MAIL_USERNAME"],
                 toaddrs=app.config["ADMINS"],
                 subject="Tempy Failure",
                 credentials=auth,
                 secure=secure,
             )
             mail_handler.setLevel(logging.ERROR)
-            app.logger.addHanlder(mail_handler)
+            app.logger.addHandler(mail_handler)
 
         if not os.path.exists("logs"):
             os.mkdir("logs")
